96cf7355cd7d3947c3eaf94e5c32ca9e0fe9179a
Scott Grisak <sgrisak@gmail.com>
2025-01-28 22:19:05 -0500

    Adding dynamic functionality with v-bind, interpolation, data and methods

--- app.js
+++ app.js
@@ -1,37 +1,55 @@
+// "Vue" is a globally available object due to the import statement
+// from the CDN. Then we need to create the app.
 Vue.createApp({
-  // because this is an object, we can write the property name
-  // 'data' that has a function as a value. It could have been
-  // written like this:
-  //data: function {...}, but this is more modern:
+  // Inside of createApp, we should find an object named "data". The
+  // object is used to configure the app. One key option is "data". The
+  // naming is required to be "data". The value stored in the data
+  // property is a function. That function is required to return an
+  // object and always an object. Not an array, not a number, not a
+  // string. Inside of here you can setup any key/value pairs.
+
+  // We use special syntax to tie the html and vue together to update
+  // the view. That syntax is applied to the html elements.
   data() {
     return {
-      goals: [],
-      // we connect the following piece to the HTML document with
-      // "v-model='enteredValue'". This allows us to take in the value
-      // the user has typed in.
-      enteredValue: "",
+      courseGoal: "Learn Vue!",
+      vueLink: "https://vuejs.org/",
     };
   },
-  // to take the enteredValue and add it to "goals", we do this:
+  // We can also use the "methods" option. It allows you to define
+  // functions that execute when something happens. This could be
+  // when you call the function or when the user does something like
+  // click a button. Methods takes an object. That object contains
+  // functions.
   methods: {
-    addGoal() {
-      this.goals.push(this.enteredValue);
-      this.enteredValue = "";
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return "Learn Vue!";
+      } else {
+        return "Master Vue!";
+      }
     },
-    // Vue establishes the connection to 'this'. The above code will be
-    // triggered when the button on the html is clicked.
+    // So now that we've created our function, how do we call it?
+    // You can call it in the html the same way we did interpolation
+    // earlier. Like this: {{ outputGoal() }}
   },
-}).mount("#app");
+}).mount("#user-goal");
 
-// const buttonEl = document.querySelector("button");
-// const inputEl = document.querySelector("input");
-// const listEl = document.querySelector("ul");
+// Could also have done:
+// const app = Vue.createApp({
+//
+// }).mount("#user-goal")
 
-// function addGoal() {
-//   const enteredValue = inputEl.value;
-//   const listItemEl = document.createElement("li");
-//   listItemEl.textContent = enteredValue;
-//   listEl.appendChild(listItemEl);
+// as well as:
+// const app = Vue.createApp({
+//     data: function() {
 // }
+// }).mount("#user-goal")
+// But what we *are* using is the method shorthand. It is more modern
+// js and should be preferred.
 
-// buttonEl.addEventListener("click", addGoal);
+// Whatever element we end up mounting with the mount method is what
+// we will control. We will also control all child elements of that
+// element. Mount expects a css selector string. This is usually an
+// ID selector.
