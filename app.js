ca4a45e0b69694a6a915690daeecf5a56fee8873
Scott Grisak <sgrisak@gmail.com>
2025-02-12 15:06:40 -0500

    Added code for native object

--- app.js
+++ app.js
@@ -18,8 +18,18 @@ const app = Vue.createApp({
     subtract(num) {
       this.counter = this.counter - num;
     },
-    setName(event) {
+    // "event" below can be any name I want. Since, in the index.html
+    // we are pointing to the *reference* to the "setName" method/function
+    // and not actually calling (or executing) the function, javascript will
+    // pass one argument: an object describing the event that occured. That
+    // one argument (or parameter) is what we are passing into the setName
+    // function.
+    setName(event, lastName) {
+      // this is just vanilla javascript. This event object that we named
+      // "event" has a target named event.target. That target has a value
+      // property that we can now access.
       this.name = event.target.value;
+      // this.name = event.target.value + " " + lastName;
     },
   },
 });
